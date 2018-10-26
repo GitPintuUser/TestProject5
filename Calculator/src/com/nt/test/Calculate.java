a63849d0c25afe3458aba4ad5dafae1c0099bcaf
SMRUTI <SMRUTI@DESKTOP-5FHVV5D>
2018-10-27 00:22:15 +0530

    Added interface calculate.java

---
+++ Calculator/src/com/nt/test/Calculate.java
@@ -0,0 +1,8 @@
+package com.nt.test;
+
+public interface Calculate {
+	public int  add(int fno,int sno);
+	public int  sub(int fno,int sno);
+	public int  mul(int fno,int sno);
+	public int  div(int fno,int sno);
+}
