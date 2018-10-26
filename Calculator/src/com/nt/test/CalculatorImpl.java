87f0ed4ec7a58ae341f1f4573054b2a2c7a3e21e
SMRUTI <SMRUTI@DESKTOP-5FHVV5D>
2018-10-27 00:57:56 +0530

    Committing add,sub,mul,div method

--- Calculator/src/com/nt/test/CalculatorImpl.java
+++ Calculator/src/com/nt/test/CalculatorImpl.java
@@ -13,4 +13,14 @@ public class CalculatorImpl implements Calculate {
 		return fno-sno;
 	}
 
+	@Override
+	public int mul(int fno, int sno) {
+		return fno*sno;
+	}
+
+	@Override
+	public int div(int fno, int sno) {
+		return fno/sno;
+	}
+
 }
